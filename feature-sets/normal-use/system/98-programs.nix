75ff773d6b132447b329d9f4043b413783d4dbd4
Aaron Kelbsch <aaron.kelbsch@anapaya.net>
2025-08-18 16:10:29 +0200

    wireshark

--- feature-sets/normal-use/system/98-programs.nix
+++ feature-sets/normal-use/system/98-programs.nix
@@ -1,4 +1,11 @@
-{vars, ...}: {
+{
+  vars,
+  pkgs,
+  ...
+}: {
   users.users.${vars.username}.extraGroups = ["wireshark"];
   programs.wireshark.enable = true;
+  environment.systemPackages = [
+    pkgs.wireshark
+  ];
 }
