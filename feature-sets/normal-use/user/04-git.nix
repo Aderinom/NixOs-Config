fd5906926c23c7fca60a4400a2ade7112ab7d996
Aaron Kelbsch <git@kelbsch.net>
2025-08-19 09:33:06 +0200

    git: settings

--- feature-sets/normal-use/user/04-git.nix
+++ feature-sets/normal-use/user/04-git.nix
@@ -9,6 +9,32 @@
     commit.gpgsign = true;
     user.signingkey = "~/.ssh/sign-key.pub";
     gpg.format = "ssh";
+
+    core.editor = "code";
+    core.ignorecase = false;
+
+    merge.conflictStyle = "zdiff3";
+
+    init.defaultBranch = "main";
+
     push.autoSetupRemote = true;
+    pull.rebase = true;
+
+    fetch.prune = true;
+
+    gc.autoDetach = true;
+    gc.writeCommitGraph = true;
+
+    rebase.autosquash = true;
+    rebase.autostash = true;
+    rebase.updateRefs = true;
+
+    # Try to rememboer how to resolve certain conflicts
+    rerere.enabled = true;
+
+    # Eagerly check objects for integrity
+    transfer.fsckObjects = true;
+    fetch.fsckObjects = true;
+    receive.fsckObjects = true;
   };
 }
