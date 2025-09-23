57c535f1f69ffdb6026eba89bd268d65baa91ea3
Aaron Kelbsch <git@kelbsch.net>
2025-09-23 09:46:47 +0200

    feat: ctf suite

---
+++ feature-sets/ctf/user/00-packages.nix
@@ -0,0 +1,40 @@
+{pkgs, ...}: {
+  home.packages = with pkgs; [
+    # Reverse Engineering
+    ghidra
+    binwalk
+
+    # Net Discovery
+    netcat
+    nmap
+    masscan
+
+    # Radio
+    urh
+    gnuradio
+
+    # Exploits
+    metasploit
+    burpsuite
+    sqlmap # SQL injection automation
+
+    # Passwords
+    john # John the Ripper, password cracker
+    hashcat # GPU-accelerated password cracker
+    aircrack-ng # WiFi key cracking
+    hydra # Network logon cracker
+    hydra-gtk # GUI for Hydra
+
+    # Web
+    amass # Subdomain enumeration
+    gobuster # Directory and DNS brute forcing
+    dirb # Web directory brute forcing
+    nikto # Web server vulnerability scanner
+
+    # General Pentesting / Utils
+    enum4linux # SMB enumeration
+    ettercap # Man-in-the-middle attack suite
+    socat # Multipurpose bidirectional data relay
+    exiftool # Metadata extractor
+  ];
+}
